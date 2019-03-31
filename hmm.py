58a7bf60fc2f3e8582739e995d30cb048dbea098
Onur Demiray <onurdemiray@Onurs-MacBook-Air.local>
2019-03-31 16:46:32 +0300

    Initial commit

---
+++ hmm.py
@@ -0,0 +1,113 @@
+from hmmlearn import hmm
+import numpy as np
+import scipy
+
+
+class HMM(object):
+
+    def __init__(self, demos, n_state):
+
+        """
+        Construction method
+
+        :param demos: List of 2D numpy arrays.
+                      Each 2D numpy array is (n_keyframe_in_the_corresponding_demo, n_dim)
+                      for action model, n_dim = 7
+                      for goal model, n_dim = 8
+
+        :param n_state: Integer, number of possible hidden states.
+        """
+
+        self.demos = demos
+        self.n_state = n_state
+
+        """
+        In this project, to learn and evaluate the Hidden Markov Model
+        hmmlearn is used. hmmlearn deals with the multiple demos by
+        taking all keyframes in one 2D Numpy array and length of all 
+        sequences. In our case lengths is a list keeping the information
+        of keyframe numbers in each demos. 
+        """
+
+        lengths = [demo.shape[0] for demo in self.demos]
+        observations = np.concatenate([demo for demo in self.demos])
+
+        self.model = hmm.GaussianHMM(n_components=self.n_state)
+        self.model.fit(observations, lengths)
+
+        """
+        Statistical Parameters Learned by using Baum_Welch
+        """
+
+        self.prior_prob = self.model.startprob_
+        self.transition_prob = self.model.transmat_
+        self.means = self.model.means_
+        self.covars = self.model.covars_
+
+    def covariance_matrix_check(self, our_tol=1e-8):
+
+        """
+        This function checks covariance matrices of all states
+        in terms of two things.
+
+        * Whether it is symmetric or not
+        * Whether it is positive semi definite or not
+
+        :param our_tol: optional, default = 1e-8
+        :return: If there is a problem raises error, otherwise nothing
+        """
+
+        def check_symmetric(a, tol=our_tol):
+            return np.allclose(a, a.T, atol=tol)
+
+        def is_psd(a, tol=our_tol):
+            E, V = scipy.linalg.eigh(a)
+            return np.all(E > -tol)
+
+        for cov in self.covars:
+            # Symmetry check
+            if not check_symmetric(cov):
+                raise ValueError('Covariance matrix must be symmetric')
+            # PSD check
+            if not is_psd(cov):
+                raise ValueError('Covariance matrix must be positive semidefinite')
+
+    def covariance_matrix_fix(self, our_tol=1e-8):
+
+        """
+        This function checks covariance matrices of all states
+        in terms of symmetry and whether it is psd or not. If
+        there is a problem it tries to fix it.
+
+        This function applies the idea proposed in the paper
+        "Computing a nearest symmetric positive semidefinite
+        matrix" (1988) by N.J. Higham.
+        """
+
+        def is_psd(a, tol=our_tol):
+            E, V = scipy.linalg.eigh(a)
+            return np.all(E > -tol)
+
+        for ind,cov in enumerate(self.covars):
+            if not is_psd(cov):
+
+                B = (cov + cov.T) / 2
+                _, s, V = np.linalg.svd(B)
+                H = np.dot(V.T, np.dot(np.diag(s), V))
+                A2 = (B + H) / 2
+                A3 = (A2 + A2.T) / 2
+
+                if is_psd(A3):
+                    self.covars[ind] = A3
+
+                else:
+
+                    spacing = np.spacing(np.linalg.norm(cov))
+                    unit = np.eye(cov.shape[0])
+                    k = 1
+                    while not is_psd(A3):
+                        mineig = np.min(np.real(np.linalg.eigvals(A3)))
+                        A3 += unit * (-mineig * k ** 2 + spacing)
+                        k += 1
+                    self.covars[ind] = A3
+
---
+++ sampling.py
@@ -0,0 +1,159 @@
+import sklearn.datasets
+import numpy as np
+import random
+
+
+def sum_normalize(arr):
+
+    """
+    This function normalizes the given 1D array so that
+    the sum of the elements in it is equal to 1. Instead
+    of inplacing, it returns a new 1D array.
+    :param arr: 1D Numpy Array
+    :return: 1D Numpy Normalized Array
+    """
+
+    return np.array([i/np.sum(arr) for i in arr])
+
+
+def random_prior_prob_generation(n_state):
+    """
+    This function returns a prior distribution based on the
+    number of states such that sum of the all probabilities
+    is equal to 1.
+
+    :param n_state: Integer
+    :return: 1D Numpy Normalized Array
+    """
+
+    return sum_normalize(np.random.uniform(0, 1, n_state))
+
+
+def random_transition_prob_generation(n_state):
+    """
+    This function returns a markov chain.
+    Properties of markov chains:
+
+    i) Each element of MC is greater than or equal to zero
+       and less than or equal to one.
+
+    ii) Sum of elements in each row has to be equal to 1.
+
+    :param nstate: Integer
+    :return: 2D Numpy Array
+    """
+
+    trans_prob = []
+    for i in range(n_state):
+        trans_prob.append(sum_normalize(np.random.uniform(0, 1, n_state)))
+
+    return np.array(trans_prob)
+
+
+def mean_vector_generation(n_state, n_dim):
+    """
+    This function return random mean vectors for each state.
+    There is not any constraint on it.
+
+    :param n_state: Integer
+    :param n_dim: Integer
+    :return: 2D Numpy Array
+    """
+
+    means = []
+    for i in range(n_state):
+        means.append(np.random.uniform(-10, +10, n_dim))
+
+    return np.array(means)
+
+
+def cov_matrix_generation(n_state, n_dim):
+    """
+    This function returns positive semidefinite covariance matrix
+    for each state by using sklearn.datasets
+
+    :param n_state: Integer
+    :param n_dim: Integer
+    :return: 3D Numpy Array
+    """
+
+    covs = np.empty(shape=(n_state, n_dim, n_dim))
+    for i in range(n_state):
+        covs[i, :] = sklearn.datasets.make_spd_matrix(n_dim)
+
+    return covs
+
+class randHMM(object):
+
+    def __init__(self, n_state, ndim):
+        """
+        :param n_state: Integer, number of hidden states
+        :param ndim: Integer, dimension of the states and observations
+        """
+
+        self.n_state = n_state
+        self.ndim = ndim
+        self.prior_probs = random_prior_prob_generation(self.n_state)
+        self.transition_probs = random_transition_prob_generation(self.n_state)
+        self.means = mean_vector_generation(self.n_state, self.ndim)
+        self.covars = cov_matrix_generation(self.n_state, self.ndim)
+
+    def sample(self, size_sample, kf_in_each_demo):
+        """
+
+        :param size_sample: integer
+        :return: list of 2D Numpy Arrays
+        """
+        demos = []
+
+        sample_size = 0
+        while sample_size < size_sample:
+
+            state_seq = []
+            current_state = which_index(self.prior_probs)
+            state_seq.append(np.random.multivariate_normal(self.means[current_state], self.covars[current_state]))
+            kf_count = 0
+            while kf_count < kf_in_each_demo[sample_size]-1:
+
+                current_state = which_index(self.transition_probs[current_state, ])
+                state_seq.append(np.random.multivariate_normal(self.means[current_state], self.covars[current_state]))
+                kf_count += 1
+
+            demos.append(np.asarray(state_seq))
+            sample_size += 1
+
+        return demos
+
+
+def which_index(arr):
+
+    sorted_ind = sorted(range(len(list(arr))), key=lambda k: arr[k])
+    cdf = []
+    for i in sorted_ind:
+        if len(cdf)==0:
+            cdf.append(arr[i])
+        else:
+            cdf.append(cdf[-1]+arr[i])
+    rand = random.uniform(0,1)
+
+    ind = 0
+    for i, p  in enumerate(cdf):
+        if p >= rand:
+            ind = i
+            break
+    return ind
+
+
+def sample_driver(n_sample, n_state_a, n_dim_a, n_state_g, n_dim_g):
+
+    action_model = randHMM(n_state_a, n_dim_a)
+    goal_model = randHMM(n_state_g, n_dim_g)
+
+    lens = []
+
+    count = 0
+    while count < n_sample:
+        lens.append(random.randint(4, 7))
+        count += 1
+
+    return action_model.sample(n_sample, lens), goal_model.sample(n_sample, lens)
